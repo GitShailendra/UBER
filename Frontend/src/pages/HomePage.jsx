c6613f1810371f379526a7ee80b229b66fcc87a1
GitShailendra <kumarshailrndra7355@gmail.com>
2025-08-06 23:38:11 +0530

    Code Pushed 6 August

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -8,12 +8,18 @@ const Home = () => {
       <div className='w-screen h-screen'>
         <img className='h-full w-full object-cover' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="" />
       </div>
-      <div className='bg-white absolute top-0 w-full'>
-        <h4>Find a trip</h4>
+      <div className='bg-white h-screen absolute top-0 w-full flex flex-col justify-end'>
+        <div className='h-[30%] bg-white p-5'>
+          <h4 className='text-2xl font-semibold'>Find a trip</h4>
         <form action="">
-          <input type="text" placeholder='Enter you pickup point' />
-          <input type="text" placeholder='Enter you destination point' />
+          <input type="text" className='bg-[#eee] px-8 py-2 text-lg rounded-lg w-full mt-5' placeholder='Enter you pickup point' />
+          <input type="text" className='bg-[#eee] px-8 py-2 text-lg rounded-lg w-full mt-3' placeholder='Enter you destination point' />
         </form>
+        </div>
+        <div className=' bg-red-500'>
+
+        </div>
+        
       </div>
     </div>
   )
